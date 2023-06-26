94b37c2ed9fd1b0740d5cce9c0a6d9d77d544980
Vandivier <vandivi3r@gmail.com>
2023-06-25 23:57:29 -0400

    feat: favicon
    and apostrophe fix

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -9,8 +9,11 @@ const Layout: BlitzLayout<{ title?: string; children?: React.ReactNode }> = ({
   return (
     <>
       <Head>
-        <title>{title || "ladderly-3"}</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>{title || "Ladderly - Accelerate Your Tech Career"}</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       {children}
