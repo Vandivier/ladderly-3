23706b538d38f8673fd71063c15d7982ebbd1ffa
Vandivier <vandivi3r@gmail.com>
2023-07-17 00:50:59 -0400

    fix: backupChecklists.js

--- scripts/backupChecklists.js
+++ scripts/backupChecklists.js
@@ -8,16 +8,33 @@ const { PrismaClient } = require("@prisma/client")
 const prisma = new PrismaClient()
 
 const backup = async () => {
-  const checklists = await prisma.checklist.findMany({
-    include: { checklistItems: true },
-    orderBy: { createdAt: "desc" },
+  const checklistNames = await prisma.checklist.groupBy({
+    by: ["name"],
   })
+
+  let checklists = []
+
+  for (const { name } of checklistNames) {
+    const checklist = await prisma.checklist.findFirst({
+      where: { name },
+      orderBy: { createdAt: "desc" },
+      include: { checklistItems: true },
+    })
+    checklists.push(checklist)
+  }
 
   const existingChecklistsRaw = fs.readFileSync(path.resolve(__dirname, "../db/checklists.json"))
   const existingChecklists = JSON.parse(existingChecklistsRaw.toString())
   const existingChecklistNames = existingChecklists.map((checklist) => checklist.name)
   const newChecklists = []
-  const premiumChecklists = []
+
+  const premiumChecklistsPath = path.resolve(__dirname, "../db/premium-checklists.json")
+  let premiumChecklists = []
+
+  if (fs.existsSync(premiumChecklistsPath)) {
+    const premiumChecklistsRaw = fs.readFileSync(premiumChecklistsPath)
+    premiumChecklists = JSON.parse(premiumChecklistsRaw.toString())
+  }
 
   for (const checklist of checklists) {
     const { name, version, checklistItems } = checklist
@@ -36,10 +53,7 @@ const backup = async () => {
     path.resolve(__dirname, "../db/checklists.json"),
     JSON.stringify(newChecklists, null, 2)
   )
-  fs.writeFileSync(
-    path.resolve(__dirname, "../db/premium-checklists.json"),
-    JSON.stringify(premiumChecklists, null, 2)
-  )
+  fs.writeFileSync(premiumChecklistsPath, JSON.stringify(premiumChecklists, null, 2))
 
   console.log("Checklist backup completed!")
 }
