a488965e165923c0126112aa4a8d9091932ddc5d
Vandivier <john@afterecon.com>
2023-07-17 19:11:08 -0400

    fix: build

--- src/checklist-items/mutations/createChecklistItem.ts
+++ src/checklist-items/mutations/createChecklistItem.ts
@@ -6,7 +6,6 @@ export default resolver.pipe(
   resolver.zod(CreateChecklistItemSchema),
   resolver.authorize(),
   async (input) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     const checklistItem = await db.checklistItem.create({ data: input })
 
     return checklistItem
--- src/checklists/mutations/createChecklist.ts
+++ src/checklists/mutations/createChecklist.ts
@@ -6,7 +6,6 @@ export default resolver.pipe(
   resolver.zod(CreateChecklistSchema),
   resolver.authorize(),
   async (input) => {
-    // TODO: in multi-tenant app, you must add validation to ensure correct tenant
     const checklist = await db.checklist.create({ data: input })
 
     return checklist
