5901b15c78681f8a0c0e55da6133281264120d3f
Vandivier <vandivi3r@gmail.com>
2023-07-21 01:24:07 -0400

    style: blog posts

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,18 +2,51 @@ import fs from "fs"
 import matter from "gray-matter"
 import { GetStaticPaths, GetStaticProps } from "next"
 import path from "path"
+import { Suspense } from "react"
 import rehypeStringify from "rehype-stringify"
 import remarkParse from "remark-parse"
 import remarkRehype from "remark-rehype"
+import styles from "src/styles/Home.module.css"
 import { unified } from "unified"
 
-import styles from "./Blog.module.css"
+import { TopNavContent } from "../index"
 
 const BlogPost = ({ title, content }: { title: string; content: string }) => {
   return (
-    <div>
-      <h1>{title}</h1>
-      <div className={styles["blog-content"]} dangerouslySetInnerHTML={{ __html: content }} />
+    <div className={styles.container}>
+      <div className="border-ladderly-light-purple flex border bg-ladderly-off-white px-4 py-1 text-ladderly-violet-700">
+        <Suspense fallback="">
+          <TopNavContent />
+        </Suspense>
+      </div>
+
+      <main className={styles.main}>
+        <h1 className="p-4 text-3xl text-ladderly-violet-600">{title}</h1>
+        <article
+          className="prose prose-lg max-w-none px-4 text-gray-700"
+          dangerouslySetInnerHTML={{ __html: content }}
+        />
+      </main>
+
+      <footer className={styles.footer}>
+        <a
+          href="https://discord.gg/fAg6Xa4uxc"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.textLink}
+        >
+          Discord
+        </a>
+        <a
+          href="https://github.com/Vandivier/ladderly-3#about"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.textLink}
+        >
+          GitHub
+        </a>
+        <p>Copyright © 2023 John Vandivier</p>
+      </footer>
     </div>
   )
 }
