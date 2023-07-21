48ba78f759eb205ba62781da0e564a0ebcde7049
Vandivier <vandivi3r@gmail.com>
2023-07-21 18:17:12 -0400

    fix: restoreUsers
    and user key syntax
    and edit page type

--- scripts/restoreUsers.js
+++ scripts/restoreUsers.js
@@ -1,14 +1,26 @@
 const path = require("path")
+const fs = require("fs-extra")
+const glob = require("glob")
 
 require("dotenv").config({ path: path.resolve(__dirname, "../.env.local") })
 
 const { PrismaClient } = require("@prisma/client")
-const fs = require("fs-extra")
 
 const prisma = new PrismaClient()
 
 async function restoreUsers() {
-  const users = await fs.readJson("./db/bak.users.json")
+  const backupFiles = glob.sync("./db/bak.users.*.json")
+
+  // Sort the files by timestamp descending
+  backupFiles.sort((a, b) => {
+    const timestampA = a.split(".").slice(-2, -1)[0]
+    const timestampB = b.split(".").slice(-2, -1)[0]
+    return new Date(timestampB) - new Date(timestampA)
+  })
+
+  const mostRecentBackupFile = backupFiles[0]
+  const users = await fs.readJson(mostRecentBackupFile)
+
   for (let user of users) {
     await prisma.user.upsert({
       where: { email: user.email },
@@ -16,7 +28,8 @@ async function restoreUsers() {
       create: user,
     })
   }
-  console.log("Restoration completed!")
+
+  console.log("User restoration completed!")
 }
 
 restoreUsers()
