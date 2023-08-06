39c3b669b6a8427e50e86d6695e6097f88aae467
Vandivier <vandivi3r@gmail.com>
2023-08-06 14:03:19 -0400

    feat: new wrapper on blog
    and community pages

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -2,24 +2,17 @@ import fs from "fs"
 import matter from "gray-matter"
 import { GetStaticPaths, GetStaticProps } from "next"
 import path from "path"
-import { Suspense } from "react"
 import rehypeStringify from "rehype-stringify"
 import remarkParse from "remark-parse"
 import remarkRehype from "remark-rehype"
 import styles from "src/styles/Home.module.css"
 import { unified } from "unified"
 
-import { TopNavContent } from "../index"
+import { LadderlyPageWrapper } from "src/core/components/page-wrapper/LadderlyPageWrapper"
 
 const BlogPost = ({ title, content }: { title: string; content: string }) => {
   return (
-    <div className={styles.container}>
-      <div className="border-ladderly-light-purple flex border bg-ladderly-off-white px-4 py-1 text-ladderly-violet-700">
-        <Suspense fallback="">
-          <TopNavContent />
-        </Suspense>
-      </div>
-
+    <LadderlyPageWrapper title={title}>
       <main className={styles.main}>
         <h1 className="p-4 text-3xl text-ladderly-violet-600">{title}</h1>
         <article
@@ -27,27 +20,7 @@ const BlogPost = ({ title, content }: { title: string; content: string }) => {
           dangerouslySetInnerHTML={{ __html: content }}
         />
       </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://discord.gg/fAg6Xa4uxc"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.textLink}
-        >
-          Discord
-        </a>
-        <a
-          href="https://github.com/Vandivier/ladderly-3#about"
-          target="_blank"
-          rel="noopener noreferrer"
-          className={styles.textLink}
-        >
-          GitHub
-        </a>
-        <p>Copyright © 2023 John Vandivier</p>
-      </footer>
-    </div>
+    </LadderlyPageWrapper>
   )
 }
 
