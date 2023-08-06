4e38ad19c258918c21ffd59dc8f0b7c3d6243b81
Vandivier <vandivi3r@gmail.com>
2023-08-06 13:37:01 -0400

    feat: home uses page wrapper

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,14 @@
-import { BlitzPage, Routes } from "@blitzjs/next"
-import { useMutation } from "@blitzjs/rpc"
+import { BlitzPage } from "@blitzjs/next"
 import { PaymentTierEnum } from "@prisma/client"
 import Image from "next/image"
 import Link from "next/link"
 import { Suspense } from "react"
 
-import logout from "src/auth/mutations/logout"
+import { LadderlyPageWrapper } from "src/core/components/page-wrapper/LadderlyPageWrapper"
 import PricingGrid from "src/core/components/pricing-grid/PricingGrid"
-import Layout from "src/core/layouts/Layout"
-import styles from "src/styles/Home.module.css"
-import { useCurrentUser } from "src/users/hooks/useCurrentUser"
 import useSubscriptionLevel from "src/users/hooks/useSubscriptionLevel"
+
+import styles from "src/styles/Home.module.css"
 
 const LadderlyHelpsContentBlock = () => {
   return (
@@ -39,59 +37,6 @@ const LadderlyHelpsContentBlock = () => {
   )
 }
 
-const LogoutButton = () => {
-  const [logoutMutation] = useMutation(logout)
-
-  return (
-    <button
-      className="ml-6"
-      onClick={async () => {
-        await logoutMutation()
-      }}
-    >
-      <strong>Logout</strong>
-    </button>
-  )
-}
-
-export const TopNavContent = () => {
-  const currentUser = useCurrentUser()
-
-  if (currentUser) {
-    return (
-      <p style={{ marginLeft: "auto" }}>
-        <Link href={Routes.BlogIndex()} className="ml-6">
-          <strong>Blog</strong>
-        </Link>
-        <Link href={Routes.CommunityPage()} className="ml-6">
-          <strong>Community</strong>
-        </Link>
-        <Link href={Routes.SettingsPage()} className="ml-6">
-          <strong>Settings</strong>
-        </Link>
-        <LogoutButton />
-      </p>
-    )
-  } else {
-    return (
-      <p style={{ marginLeft: "auto" }}>
-        <Link href={Routes.BlogIndex()} className="ml-6">
-          <strong>Blog</strong>
-        </Link>
-        <Link href={Routes.CommunityPage()} className="ml-6">
-          <strong>Community</strong>
-        </Link>
-        <Link href={Routes.LoginPage()} className="ml-6">
-          <strong>Login</strong>
-        </Link>
-        <Link href={Routes.SignupPage()} className="ml-6">
-          <strong>Signup</strong>
-        </Link>
-      </p>
-    )
-  }
-}
-
 const AdvancedChecklistContentBlock = () => {
   const { tier } = useSubscriptionLevel()
   const isPaid = tier != PaymentTierEnum.FREE
@@ -117,101 +62,73 @@ const AdvancedChecklistContentBlock = () => {
 
 const Home: BlitzPage = () => {
   return (
-    <Layout title="Home">
+    <LadderlyPageWrapper title="Home">
       <div className={styles.globe} />
 
-      <div className={styles.container}>
-        <div className="border-ladderly-light-purple flex border bg-ladderly-off-white px-4 py-1 text-ladderly-violet-700">
-          <Suspense fallback="">
-            <TopNavContent />
-          </Suspense>
-        </div>
-
-        <main className={styles.main}>
-          <div className={styles.wrapper}>
-            <div className={styles.header}>
-              <div>
-                <div className={styles.logo}>
-                  <Image src="/logo.png" alt="Ladderly Logo" width={300} height={300} />
-                </div>
-                <h2 className="text-2xl font-bold text-gray-800">Accelerate Your Tech Career</h2>
-              </div>
-              <LadderlyHelpsContentBlock />
-            </div>
-
+      <main className={styles.main}>
+        <div className={styles.wrapper}>
+          <div className={styles.header}>
             <div>
-              <div className={`${styles.nextStepsCard} rounded-lg bg-white p-6 shadow-lg`}>
-                <h2 className="text-2xl font-bold text-gray-800" style={{ marginBottom: "0.5rem" }}>
-                  Recommended Next Steps:
-                </h2>
-                <h2 className="text-2xl font-bold text-gray-800">
-                  Complete the{" "}
-                  <Link
-                    className="text-2xl font-bold text-ladderly-pink hover:underline"
-                    href={"/checklists/my-basic-checklist"}
-                  >
-                    Standard Checklist
-                  </Link>
-                  , <span className="text-2xl font-bold">consider one of the paid plans below</span>
-                  , and{" "}
-                  <Link
-                    className="text-2xl font-bold text-ladderly-pink hover:underline"
-                    href={"https://buy.stripe.com/cN2bMfbOQ2CX5dC7su"}
-                    target="_blank"
-                  >
-                    Book an Expert Session
-                  </Link>
-                  !
-                </h2>
+              <div className={styles.logo}>
+                <Image src="/logo.png" alt="Ladderly Logo" width={300} height={300} />
               </div>
-
-              <div
-                className={`${styles.nextStepsCard} rounded-lg bg-white p-2 shadow-lg`}
-                style={{ marginTop: "0.5rem" }}
-              >
-                <h3 className="text-m font-bold text-gray-800">
-                  To support Ladderly{"'"}s mission to provide low-cost education in STEM, consider{" "}
-                  <Link
-                    className="text-m font-bold text-ladderly-pink hover:underline"
-                    href={"https://buy.stripe.com/eVa9E72egelFfSgfYZ"}
-                    target="_blank"
-                  >
-                    leaving a tip
-                  </Link>
-                  .
-                </h3>
-              </div>
-
-              <Suspense>
-                <AdvancedChecklistContentBlock />
-              </Suspense>
-
-              <PricingGrid />
+              <h2 className="text-2xl font-bold text-gray-800">Accelerate Your Tech Career</h2>
             </div>
+            <LadderlyHelpsContentBlock />
           </div>
-        </main>
 
-        <footer className={styles.footer}>
-          <a
-            href="https://discord.gg/fAg6Xa4uxc"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.textLink}
-          >
-            Discord
-          </a>
-          <a
-            href="https://github.com/Vandivier/ladderly-3#about"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.textLink}
-          >
-            GitHub
-          </a>
-          <p>Copyright © 2023 John Vandivier</p>
-        </footer>
-      </div>
-    </Layout>
+          <div>
+            <div className={`${styles.nextStepsCard} rounded-lg bg-white p-6 shadow-lg`}>
+              <h2 className="text-2xl font-bold text-gray-800" style={{ marginBottom: "0.5rem" }}>
+                Recommended Next Steps:
+              </h2>
+              <h2 className="text-2xl font-bold text-gray-800">
+                Complete the{" "}
+                <Link
+                  className="text-2xl font-bold text-ladderly-pink hover:underline"
+                  href={"/checklists/my-basic-checklist"}
+                >
+                  Standard Checklist
+                </Link>
+                , <span className="text-2xl font-bold">consider one of the paid plans below</span>,
+                and{" "}
+                <Link
+                  className="text-2xl font-bold text-ladderly-pink hover:underline"
+                  href={"https://buy.stripe.com/cN2bMfbOQ2CX5dC7su"}
+                  target="_blank"
+                >
+                  Book an Expert Session
+                </Link>
+                !
+              </h2>
+            </div>
+
+            <div
+              className={`${styles.nextStepsCard} rounded-lg bg-white p-2 shadow-lg`}
+              style={{ marginTop: "0.5rem" }}
+            >
+              <h3 className="text-m font-bold text-gray-800">
+                To support Ladderly{"'"}s mission to provide low-cost education in STEM, consider{" "}
+                <Link
+                  className="text-m font-bold text-ladderly-pink hover:underline"
+                  href={"https://buy.stripe.com/eVa9E72egelFfSgfYZ"}
+                  target="_blank"
+                >
+                  leaving a tip
+                </Link>
+                .
+              </h3>
+            </div>
+
+            <Suspense>
+              <AdvancedChecklistContentBlock />
+            </Suspense>
+
+            <PricingGrid />
+          </div>
+        </div>
+      </main>
+    </LadderlyPageWrapper>
   )
 }
 
