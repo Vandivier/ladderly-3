8d7b00d2057842a5dba460a816fcdb9da6a637b8
Vandivier <vandivi3r@gmail.com>
2023-08-06 13:33:52 -0400

    feat: page wrapper home icon

--- src/core/components/page-wrapper/LadderlyPageWrapper.tsx
+++ src/core/components/page-wrapper/LadderlyPageWrapper.tsx
@@ -5,9 +5,10 @@ import React from "react"
 
 import logout from "src/auth/mutations/logout"
 import { useCurrentUser } from "src/users/hooks/useCurrentUser"
-import Layout, { LayoutProps } from "../layouts/Layout"
+import Layout, { LayoutProps } from "../../layouts/Layout"
 
 import styles from "src/styles/Home.module.css"
+import { HomeIcon } from "./HomeIcon"
 
 const LogoutButton = () => {
   const [logoutMutation] = useMutation(logout)
@@ -29,18 +30,24 @@ export const TopNavContent = () => {
 
   if (currentUser) {
     return (
-      <p style={{ marginLeft: "auto" }}>
-        <Link href={Routes.BlogIndex()} className="ml-6">
-          <strong>Blog</strong>
+      <>
+        <Link href={Routes.Home()} style={{ marginRight: "auto" }}>
+          <HomeIcon />
         </Link>
-        <Link href={Routes.CommunityPage()} className="ml-6">
-          <strong>Community</strong>
-        </Link>
-        <Link href={Routes.SettingsPage()} className="ml-6">
-          <strong>Settings</strong>
-        </Link>
-        <LogoutButton />
-      </p>
+
+        <p style={{ marginLeft: "auto" }}>
+          <Link href={Routes.BlogIndex()} className="ml-6">
+            <strong>Blog</strong>
+          </Link>
+          <Link href={Routes.CommunityPage()} className="ml-6">
+            <strong>Community</strong>
+          </Link>
+          <Link href={Routes.SettingsPage()} className="ml-6">
+            <strong>Settings</strong>
+          </Link>
+          <LogoutButton />
+        </p>
+      </>
     )
   } else {
     return (
@@ -65,7 +72,7 @@ export const TopNavContent = () => {
 export const LadderlyPageWrapper: React.FC<LayoutProps> = ({ children, title }) => (
   <Layout title={title}>
     <div className={styles.container}>
-      <div className="border-ladderly-light-purple flex border bg-ladderly-off-white px-4 py-1 text-ladderly-violet-700">
+      <div className="border-ladderly-light-purple flex items-center border bg-ladderly-off-white px-4 py-1 text-ladderly-violet-700">
         <React.Suspense fallback="">
           <TopNavContent />
         </React.Suspense>
--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -1,22 +1,20 @@
-import { Routes } from "@blitzjs/next"
 import { useMutation, useQuery } from "@blitzjs/rpc"
 import { FORM_ERROR } from "final-form"
 import Head from "next/head"
-import Link from "next/link"
 import { Suspense } from "react"
 
-import Layout from "src/core/layouts/Layout"
 import { SettingForm } from "src/settings/components/SettingForm"
 import updateSettingsMutation from "src/settings/mutations/updateSettingsMutation"
 import getSettings, { UserSettings } from "src/settings/queries/getSettings"
 import { UpdateSettingsSchema } from "src/settings/schemas"
+import { LadderlyPageWrapper } from "src/core/components/page-wrapper/LadderlyPageWrapper"
 
 export const SettingsList = () => {
   const [settings, { setQueryData }] = useQuery(getSettings, {})
   const [updateSettings] = useMutation(updateSettingsMutation)
 
   return (
-    <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
+    <div className="my-4 w-full max-w-md rounded-lg bg-white p-8 shadow-md">
       <h1 className="mb-4 text-2xl font-bold text-gray-800">Edit Settings & Profile</h1>
       <p>Please email john@ladderly.io to update your subscription tier.</p>
 
@@ -51,24 +49,17 @@ export const SettingsList = () => {
 
 const SettingsPage = () => {
   return (
-    <Layout>
+    <LadderlyPageWrapper>
       <Head>
         <title>Settings</title>
       </Head>
 
-      <div className="relative min-h-screen">
-        <nav className="border-ladderly-light-purple flex border bg-ladderly-off-white px-4 py-1 text-ladderly-violet-700">
-          <Link href={Routes.Home()} className="ml-auto text-gray-800 hover:text-ladderly-pink">
-            Back to Home
-          </Link>
-        </nav>
-        <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center">
-          <Suspense fallback={<div>Loading...</div>}>
-            <SettingsList />
-          </Suspense>
-        </div>
+      <div className="flex items-center justify-center">
+        <Suspense fallback={<div>Loading...</div>}>
+          <SettingsList />
+        </Suspense>
       </div>
-    </Layout>
+    </LadderlyPageWrapper>
   )
 }
 
