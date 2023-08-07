022e444ed6440f5962faee41101f5cd68927d7d7
Vandivier <vandivi3r@gmail.com>
2023-08-06 23:17:45 -0400

    feat: nav menus

--- src/core/components/page-wrapper/TopNav.tsx
+++ src/core/components/page-wrapper/TopNav.tsx
@@ -1,11 +1,14 @@
-import { TopNavCenter } from "./TopNavCenter"
+import React from "react"
+
 import { TopNavLeft } from "./TopNavLeft"
-import { TopNavRight } from "./TopNavRight"
+import { TopNavRight, TopNavRightSkeleton } from "./TopNavRight"
 
 export const TopNav = () => (
   <div className="border-ladderly-light-purple flex items-center border bg-ladderly-off-white px-4 py-1 text-ladderly-violet-700">
     <TopNavLeft />
-    <TopNavCenter />
-    {/* <TopNavRight /> */}
+
+    <React.Suspense fallback={<TopNavRightSkeleton />}>
+      <TopNavRight />
+    </React.Suspense>
   </div>
 )
