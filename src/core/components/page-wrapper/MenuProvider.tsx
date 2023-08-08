76df1e411d31543751c3cf1f84da62135103a64d
Vandivier <vandivi3r@gmail.com>
2023-08-07 21:55:00 -0400

    feat: setMenu

--- src/core/components/page-wrapper/MenuProvider.tsx
+++ src/core/components/page-wrapper/MenuProvider.tsx
@@ -1,19 +1,29 @@
 import React from "react"
 
 type MenuContextType = {
-  menuContent: React.ReactNode | null
-  setMenuContent: (node: React.ReactNode | null) => void
+  menuContent: React.ReactNode
+  openMenuName: string | null
+  setMenu: (menuContent: React.ReactNode, openMenuName: string) => void
 }
 
 export const MenuContext = React.createContext<MenuContextType>({
   menuContent: null,
-  setMenuContent: () => {},
+  openMenuName: null,
+  setMenu: (menuContent: React.ReactNode, openMenuName: string) => {},
 })
 
 export const MenuProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
-  const [menuContent, setMenuContent] = React.useState<React.ReactNode | null>(null)
+  const [menuContent, setMenuContent] = React.useState<React.ReactNode>(null)
+  const [openMenuName, setOpenMenuName] = React.useState<string | null>(null)
+
+  const setMenu = (menuContent: React.ReactNode, openMenuName: string) => {
+    setMenuContent(menuContent)
+    setOpenMenuName(openMenuName)
+  }
 
   return (
-    <MenuContext.Provider value={{ menuContent, setMenuContent }}>{children}</MenuContext.Provider>
+    <MenuContext.Provider value={{ menuContent, openMenuName, setMenu }}>
+      {children}
+    </MenuContext.Provider>
   )
 }
