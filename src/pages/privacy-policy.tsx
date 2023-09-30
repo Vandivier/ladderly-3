bc5b0247ff588b9c10540403eaaa5301fba894e3
Vandivier <vandivi3r@gmail.com>
2023-09-30 15:16:30 -0400

    feat: privacy policy content

--- src/pages/privacy-policy.tsx
+++ src/pages/privacy-policy.tsx
@@ -2,21 +2,19 @@ import { BlitzPage } from "@blitzjs/next"
 import { LargeCard } from "src/core/components/LargeCard"
 import { LadderlyPageWrapper } from "src/core/components/page-wrapper/LadderlyPageWrapper"
 
+import styles from "src/styles/Home.module.css"
+
 const PrivacyPolicyPage: BlitzPage = () => {
   return (
     <LadderlyPageWrapper title="Ladderly | Privacy Policy">
       <LargeCard>
-        {/* <div className="container mx-auto mt-5 px-4"> */}
-        <h1 className="mb-4 text-2xl font-semibold">
-          ladderly.io Privacy Policy: Questions and Answers
-        </h1>
-
-        <p>We take your privacy seriously. And we give you full control over your data.</p>
+        <h1 className="mb-4 text-2xl font-semibold">Ladderly Privacy Policy</h1>
 
         <h2 className="mb-3 mt-5 text-xl">Does ladderly.io collect anonymous data?</h2>
         <p>
           When you use the ladderly.io website, we may collect some anonymous data to understand how
-          users interact with ladderly.io and insights such as the browser they are using.
+          users interact with ladderly.io site. Such information may include the browser you are
+          using and which pages you have visited.
         </p>
 
         <h2 className="mb-3 mt-5 text-xl">
@@ -24,7 +22,8 @@ const PrivacyPolicyPage: BlitzPage = () => {
         </h2>
         <p>
           If you create a ladderly.io account, we collect some personal data to track your progress
-          and to customize your user experience.
+          and to customize your user experience. If you make a purchase in the ladderly.io store, we
+          collect data to ensure proper delivery and for tax accounting reasons.
         </p>
 
         <h2 className="mb-3 mt-5 text-xl">Can I use ladderly.io anonymously?</h2>
@@ -35,39 +34,45 @@ const PrivacyPolicyPage: BlitzPage = () => {
 
         <h2 className="mb-3 mt-5 text-xl">If I create an account, what data will you collect?</h2>
         <p>
-          We{`'`}ll request your email address for sign-in purposes and occasional updates. You can
-          also add more about yourself in your user profile, though this is entirely optional.
+          A valid email address is required to create an account. You can also add more information
+          about yourself in your user profile, though this is entirely optional.
         </p>
-
-        <h2 className="mb-3 mt-5 text-xl">
-          You said I have full control over my data. What does that mean, exactly?
-        </h2>
-        <p>
-          You can download all your data in JSON format anytime, control your profile visibility,
-          and delete any personal data or even your entire account.
-        </p>
-
-        {/* ... Other Q&A sections ... */}
 
         <h2 className="mb-3 mt-5 text-xl">Can any other organizations access my data?</h2>
         <p>
-          We don{`'`}t sell your data. We utilize services like AWS and Auth0, and while your data
-          may pass through them, you can always check their respective privacy policies for
-          assurance.
+          We do not sell your data, though we do utilize some third party tools. For example,
+          ladderly.io utilizes{" "}
+          <a
+            href="https://vercel.com"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.textLink}
+          >
+            Vercel
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://supabase.com/"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.textLink}
+          >
+            Supabase
+          </a>
+          . You can check their respective privacy policies for assurance.
         </p>
 
-        <h2 className="mb-3 mt-5 text-xl">I have questions about my privacy on ladderly.io.</h2>
+        <h2 className="mb-3 mt-5 text-xl">I have questions about my privacy on Ladderly</h2>
         <p>
           We{`'`}re here to help. Reach out at{" "}
-          <a href="mailto:privacy@ladderly.io">privacy@ladderly.io</a>.
+          <a href="mailto:admin@ladderly.io">admin@ladderly.io</a>.
         </p>
 
         <h2 className="mb-3 mt-5 text-xl">How can I find out about changes?</h2>
         <p>
-          This version of the ladderly.io privacy Q&A was last updated on [insert latest date here].
-          Keep an eye on your emails for any future updates or changes.
+          This version of the ladderly.io privacy Q&A was last updated on 9/30/2023. Keep an eye on
+          your emails for any future updates or changes.
         </p>
-        {/* </div> */}
       </LargeCard>
     </LadderlyPageWrapper>
   )
