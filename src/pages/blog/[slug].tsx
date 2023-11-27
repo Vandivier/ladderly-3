1d560dfd9a47fb5ffe0e49c40db9a7801997efc0
Vandivier <vandivi3r@gmail.com>
2023-11-26 21:49:48 -0500

    docs: Ladderly Leetcode Kata

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -10,6 +10,19 @@ import { unified } from "unified"
 
 import { LadderlyPageWrapper } from "src/core/components/page-wrapper/LadderlyPageWrapper"
 
+const tipUrl =
+  "https://checkout.stripe.com/c/pay/cs_live_a10YyjvS4xEbZJ9Th74ZTitGd2NZoHBILwU0K8AdL1P5INh5a9ry7h1Bj9#fidkdWxOYHwnPyd1blppbHNgWlIzUk5qNVddT2AyYGM8PURQN01fTUFSYjU1bX9nPX1KZ1InKSd1aWxrbkB9dWp2YGFMYSc%2FJ2BTZDxAMjdgYmBpQ2NWYmNcXycpJ3dgY2B3d2B3SndsYmxrJz8nbXFxdXY%2FKipycnIraWRhYWB3aXwrbGoqJ3gl"
+
+const sBlogCallToAction = `<p class="call-to-action"
+  style="font-size: 1rem; font-style: italic; margin: 0 auto;">
+  
+    Thanks for reading! Ladderly is open source and community driven.
+    Consider supporting the maintainers by
+    <a href="${tipUrl}" target="_blank">
+      leaving a tip!
+    </a>
+  </p>`
+
 const BlogPost = ({ title, content }: { title: string; content: string }) => {
   return (
     <LadderlyPageWrapper title={title}>
@@ -56,10 +69,12 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
     .use(rehypeStringify, { allowDangerousHtml: true })
     .process(content)
 
+  const updatedHtml = htmlContent.toString().replaceAll("{{ BlogCallToAction }}", sBlogCallToAction)
+
   return {
     props: {
       title: data.title,
-      content: htmlContent.toString(),
+      content: updatedHtml,
     },
   }
 }
