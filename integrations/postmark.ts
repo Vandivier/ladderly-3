682924e4432903fde162ba4ef51ba37ffad4780b
Vandivier <vandivi3r@gmail.com>
2024-01-10 20:05:03 -0500

    fix: reset pass

---
+++ integrations/postmark.ts
@@ -0,0 +1,3 @@
+import { ServerClient } from "postmark"
+
+export const postmarkClient = new ServerClient(process.env.POSTMARK_API_KEY || "")
--- mailers/forgotPasswordMailer.ts
+++ mailers/forgotPasswordMailer.ts
@@ -1,9 +1,4 @@
-/* TODO - You need to add a mailer integration in `integrations/` and import here.
- *
- * The integration file can be very simple. Instantiate the email client
- * and then export it. That way you can import here and anywhere else
- * and use it straight away.
- */
+import { postmarkClient } from "integrations/postmark"
 
 type ResetPasswordMailer = {
   to: string
@@ -11,30 +6,30 @@ type ResetPasswordMailer = {
 }
 
 export function forgotPasswordMailer({ to, token }: ResetPasswordMailer) {
-  // In production, set APP_ORIGIN to your production server origin
   const origin = process.env.APP_ORIGIN || process.env.BLITZ_DEV_SERVER_ORIGIN
   const resetUrl = `${origin}/auth/reset-password?token=${token}`
 
+  const html = `
+  <h1>Reset Your Password</h1>
+  <a href="${resetUrl}">
+    Click here to set a new password
+  </a>
+`
+
   const msg = {
-    from: "TODO@example.com",
+    from: "support@ladderly.io",
+    From: "support@ladderly.io",
     to,
     subject: "Your Password Reset Instructions",
-    html: `
-      <h1>Reset Your Password</h1>
-      <h3>NOTE: You must set up a production email integration in mailers/forgotPasswordMailer.ts</h3>
-
-      <a href="${resetUrl}">
-        Click here to set a new password
-      </a>
-    `,
+    Subject: "Your Password Reset Instructions",
+    html,
+    HtmlBody: html,
   }
 
   return {
     async send() {
-      if (process.env.NODE_ENV === "production") {
-        // TODO - send the production email, like this:
-        // await postmark.sendEmail(msg)
-        throw new Error("No production email implementation in mailers/forgotPasswordMailer")
+      if (typeof process.env.POSTMARK_API_KEY === "string" && process.env.POSTMARK_API_KEY !== "") {
+        await postmarkClient.sendEmail(msg)
       } else {
         // Preview email in the browser
         const previewEmail = (await import("preview-email")).default
