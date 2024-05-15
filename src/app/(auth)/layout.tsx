72c368c468ac29fc15c54261acadd34af1af45ce
Vandivier <vandivi3r@gmail.com>
2024-05-14 22:22:48 -0400

    feat: commit app components

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,12 @@
+import { useAuthenticatedBlitzContext } from "../blitz-server"
+
+export default async function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  await useAuthenticatedBlitzContext({
+    redirectAuthenticatedTo: "/",
+  })
+  return <>{children}</>
+}
