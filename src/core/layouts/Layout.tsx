d1a2ffe435d505b89ac825749eb97cb0c19428ec
Vandivier <vandivi3r@gmail.com>
2024-05-14 22:54:02 -0400

    fix: temp use compat router

--- src/core/layouts/Layout.tsx
+++ src/core/layouts/Layout.tsx
@@ -1,22 +1,36 @@
 import Head from "next/head"
 import React from "react"
 import { BlitzLayout } from "@blitzjs/next"
-import { useRouter } from "next/router"
+import { useRouter } from "next/compat/router"
 
 export type LayoutProps = { title: string; children?: React.ReactNode }
 
 const Layout: BlitzLayout<LayoutProps> = ({ title, children }) => {
   const router = useRouter()
-  const canonical = `https://ladderly.io${router.asPath}`
+  const canonical = `https://ladderly.io${router?.asPath}`
   const preppendedTitle = `ladderly.io | ${title}`
 
   return (
     <>
       <Head>
         <title>{preppendedTitle}</title>
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
         <link rel="manifest" href="/site.webmanifest" />
         <link rel="canonical" href={canonical} key="canonical" />
 
