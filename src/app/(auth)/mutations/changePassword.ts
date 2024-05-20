aea5cb322b31f09079ed500982d57b1e08fb82c3
Vandivier <vandivi3r@gmail.com>
2024-05-19 22:43:25 -0400

    feat: migrate change pass

--- src/app/(auth)/mutations/changePassword.ts
+++ src/app/(auth)/mutations/changePassword.ts
@@ -1,9 +1,9 @@
-import { NotFoundError } from "blitz"
-import db from "db"
-import { authenticateUser } from "./login"
-import { ChangePassword } from "../schemas"
-import { resolver } from "@blitzjs/rpc"
 import { SecurePassword } from "@blitzjs/auth/secure-password"
+import { resolver } from "@blitzjs/rpc"
+import { AuthenticationError, NotFoundError } from "blitz"
+import db from "db"
+import { ChangePassword } from "../schemas"
+import { authenticateUser } from "./login"
 
 export default resolver.pipe(
   resolver.zod(ChangePassword),
@@ -11,7 +11,16 @@ export default resolver.pipe(
   async ({ currentPassword, newPassword }, ctx) => {
     const user = await db.user.findFirst({ where: { id: ctx.session.userId } })
     if (!user) throw new NotFoundError()
-    await authenticateUser(user.email, currentPassword)
+
+    try {
+      await authenticateUser(user.email, currentPassword)
+    } catch (error) {
+      if (error instanceof AuthenticationError) {
+        throw new Error("Invalid Password")
+      }
+      throw error
+    }
+
     const hashedPassword = await SecurePassword.hash(newPassword.trim())
     await db.user.update({
       where: { id: user.id },
