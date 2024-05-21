30c6cc424c78ebde6991b4bdd50480a95e04a390
Vandivier <vandivi3r@gmail.com>
2024-05-21 16:47:08 -0400

    fix: async issue on home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -65,95 +65,95 @@ const AdvancedChecklistContentBlock = () => {
   ) : null
 }
 
-export default async function Home() {
-  return (
-    <LadderlyPageWrapper title="Home">
-      <main style={{ padding: "0rem 1rem" }}>
-        <div className={styles.wrapper}>
-          <div
-            className={`mx-auto my-6 flex flex-wrap gap-0 rounded-lg bg-frost p-2 sm:flex-nowrap sm:gap-16`}
-          >
-            <Image
-              alt="Ladderly Logo"
-              className="m-auto block rounded-lg shadow-lg sm:hidden"
-              height={270}
-              priority={true}
-              src="/logo.png"
-              width={270}
-            />
+const HomePage = () => (
+  <LadderlyPageWrapper title="Home">
+    <main style={{ padding: "0rem 1rem" }}>
+      <div className={styles.wrapper}>
+        <div
+          className={`mx-auto my-6 flex flex-wrap gap-0 rounded-lg bg-frost p-2 sm:flex-nowrap sm:gap-16`}
+        >
+          <Image
+            alt="Ladderly Logo"
+            className="m-auto block rounded-lg shadow-lg sm:hidden"
+            height={270}
+            priority={true}
+            src="/logo.png"
+            width={270}
+          />
 
-            <Image
-              alt="Ladderly Logo"
-              height={330}
-              className="m-6 hidden rounded-lg shadow-lg sm:block"
-              priority={true}
-              src="/logo.png"
-              width={330}
-            />
+          <Image
+            alt="Ladderly Logo"
+            height={330}
+            className="m-6 hidden rounded-lg shadow-lg sm:block"
+            priority={true}
+            src="/logo.png"
+            width={330}
+          />
 
-            <LadderlyHelpsContentBlock />
-          </div>
-
-          <div>
-            <div
-              className={`${styles.nextStepsCard} rounded-lg bg-white p-6 shadow-lg`}
-            >
-              <h2
-                className="text-2xl font-bold text-gray-800"
-                style={{ marginBottom: "0.5rem" }}
-              >
-                Recommended Next Steps:
-              </h2>
-              <h2 className="text-2xl font-bold text-gray-800">
-                Complete the{" "}
-                <Link
-                  className="text-2xl font-bold text-ladderly-pink hover:underline"
-                  href={"/checklists/my-basic-checklist"}
-                >
-                  Standard Checklist
-                </Link>
-                ,{" "}
-                <span className="text-2xl font-bold">
-                  consider one of the paid plans below
-                </span>
-                , and{" "}
-                <Link
-                  className="text-2xl font-bold text-ladderly-pink hover:underline"
-                  href={"https://buy.stripe.com/cN2bMfbOQ2CX5dC7su"}
-                  target="_blank"
-                >
-                  Book an Expert Session
-                </Link>
-                !
-              </h2>
-            </div>
-
-            <div
-              className={`${styles.nextStepsCard} rounded-lg bg-white p-2 shadow-lg`}
-              style={{ marginTop: "0.5rem" }}
-            >
-              <h3 className="text-m font-bold text-gray-800">
-                To support Ladderly{"'"}s mission to provide low-cost education
-                in STEM, consider{" "}
-                <Link
-                  className="text-m font-bold text-ladderly-pink hover:underline"
-                  href={"https://buy.stripe.com/eVa9E72egelFfSgfYZ"}
-                  target="_blank"
-                >
-                  leaving a tip
-                </Link>
-                .
-              </h3>
-            </div>
-
-            <Suspense>
-              <AdvancedChecklistContentBlock />
-            </Suspense>
-
-            <PricingGrid />
-          </div>
+          <LadderlyHelpsContentBlock />
         </div>
-      </main>
-    </LadderlyPageWrapper>
-  )
-}
+
+        <div>
+          <div
+            className={`${styles.nextStepsCard} rounded-lg bg-white p-6 shadow-lg`}
+          >
+            <h2
+              className="text-2xl font-bold text-gray-800"
+              style={{ marginBottom: "0.5rem" }}
+            >
+              Recommended Next Steps:
+            </h2>
+            <h2 className="text-2xl font-bold text-gray-800">
+              Complete the{" "}
+              <Link
+                className="text-2xl font-bold text-ladderly-pink hover:underline"
+                href={"/checklists/my-basic-checklist"}
+              >
+                Standard Checklist
+              </Link>
+              ,{" "}
+              <span className="text-2xl font-bold">
+                consider one of the paid plans below
+              </span>
+              , and{" "}
+              <Link
+                className="text-2xl font-bold text-ladderly-pink hover:underline"
+                href={"https://buy.stripe.com/cN2bMfbOQ2CX5dC7su"}
+                target="_blank"
+              >
+                Book an Expert Session
+              </Link>
+              !
+            </h2>
+          </div>
+
+          <div
+            className={`${styles.nextStepsCard} rounded-lg bg-white p-2 shadow-lg`}
+            style={{ marginTop: "0.5rem" }}
+          >
+            <h3 className="text-m font-bold text-gray-800">
+              To support Ladderly{"'"}s mission to provide low-cost education in
+              STEM, consider{" "}
+              <Link
+                className="text-m font-bold text-ladderly-pink hover:underline"
+                href={"https://buy.stripe.com/eVa9E72egelFfSgfYZ"}
+                target="_blank"
+              >
+                leaving a tip
+              </Link>
+              .
+            </h3>
+          </div>
+
+          <Suspense>
+            <AdvancedChecklistContentBlock />
+          </Suspense>
+
+          <PricingGrid />
+        </div>
+      </div>
+    </main>
+  </LadderlyPageWrapper>
+)
+
+export default HomePage
