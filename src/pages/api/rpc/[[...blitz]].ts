b119fa2c85caa9d02d8cf99add3a4c3c4532fa46
Vandivier <vandivi3r@gmail.com>
2024-05-31 14:59:49 -0400

    feat: disable debug logs

--- src/pages/api/rpc/[[...blitz]].ts
+++ src/pages/api/rpc/[[...blitz]].ts
@@ -3,4 +3,11 @@
 import { rpcHandler } from "@blitzjs/rpc"
 import { api } from "src/app/blitz-server"
 
-export default api(rpcHandler({ onError: (error, ctx) => console.log(error) }))
+export default api(
+  rpcHandler({
+    onError: (error, ctx) => console.log(error),
+    logging: {
+      disablelevel: "debug",
+    },
+  })
+)
