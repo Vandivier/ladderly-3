f1073ef665635cfea423c58fe367e34d69b3c4fa
Vandivier <vandivi3r@gmail.com>
2024-08-14 15:08:16 -0400

    feat: more generic VotableTypeEnum

--- src/app/top-honors/page.tsx
+++ src/app/top-honors/page.tsx
@@ -7,21 +7,29 @@ import { VotableTypeSelector } from 'src/votable/VotableTypeSelector'
 import getVotableLeaders from './queries/getVotableLeaders'
 
 const toTitleCase = (str) =>
-  str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+  str
+    .split('_')
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ')
 
 const VotableLeaders = ({ type }) => {
   const [leaders] = useQuery(getVotableLeaders, { type })
 
   return (
     <div>
-      <h2 className="mb-4 text-xl font-bold">
+      <h2 className="mb-4 text-center text-2xl font-bold">
         Top {`${toTitleCase(type)}`} Honors
       </h2>
+      <p className="my-4 max-w-[400px] text-sm">
+        These are the top 10 {`${toTitleCase(type)}`} leaders based on their
+        prestige score and registered user votes. Ties are sorted by name.
+      </p>
       <ul>
         {leaders.map((leader) => (
           <li key={leader.id}>
-            {leader.name} - {leader.prestigeScore} points (
-            {leader.registeredUserVotes} registered votes)
+            <span className="font-bold">{leader.name}</span> -{' '}
+            {leader.prestigeScore} points ({leader.registeredUserVotes}{' '}
+            registered votes)
           </li>
         ))}
       </ul>
@@ -34,14 +42,14 @@ export default function TopHonorsPage() {
 
   return (
     <LadderlyPageWrapper title="Top Honors">
-      <div className="flex flex-col items-center justify-center">
-        <div className="m-4">
+      <main className="m-4 flex flex-col items-center justify-center">
+        <div className="m-auto my-4">
           <VotableTypeSelector value={votableType} onChange={setVotableType} />
         </div>
         <Suspense fallback={<div>Loading...</div>}>
           <VotableLeaders type={votableType} />
         </Suspense>
-      </div>
+      </main>
     </LadderlyPageWrapper>
   )
 }
