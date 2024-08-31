0fccc1c314397b603f7c236a7825b2cb2c554c61
Vandivier <vandivi3r@gmail.com>
2024-08-30 21:55:51 -0400

    feat: create comments

---
+++ blitz-app/src/app/comments/createComment.ts
@@ -0,0 +1,19 @@
+import db from 'db'
+
+async function createComment(
+  authorId: number,
+  parentVotableId: number,
+  body: string
+) {
+  const comment = await db.votable.create({
+    data: {
+      type: 'COMMENT',
+      name: `Comment on ${parentVotableId}`,
+      body,
+      author: { connect: { id: authorId } },
+      parentVotable: { connect: { id: parentVotableId } },
+    },
+  })
+
+  return comment
+}
