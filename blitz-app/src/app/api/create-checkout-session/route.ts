2e38023ce9fe4a76b4a281afd33c1895e46b88eb
Vandivier <vandivi3r@gmail.com>
2024-09-29 13:33:46 -0400

    feat: checkout session route

---
+++ blitz-app/src/app/api/create-checkout-session/route.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from 'next/server'
+import Stripe from 'stripe'
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+  apiVersion: '2024-06-20',
+})
+
+export async function POST(req: Request) {
+  try {
+    // Optionally, parse any request body data if needed
+    const { priceId } = await req.json()
+
+    // Create the Checkout session parameters
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ['card'],
+      mode: 'subscription',
+      line_items: [
+        {
+          price: priceId,
+          quantity: 1,
+        },
+      ],
+      success_url: `${process.env.NEXT_PUBLIC_SITE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: `${process.env.NEXT_PUBLIC_SITE_URL}/cancel`,
+    })
+
+    return NextResponse.json({ sessionId: session.id })
+  } catch (err: any) {
+    console.error(err.message)
+    return NextResponse.json({ error: err.message }, { status: 500 })
+  }
+}
