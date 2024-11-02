a8d0a37780ec6b5eda2de69ed3869f50248e1a06
Vandivier <john@ladderly.io>
2024-11-02 19:03:53 -0400

    fix: mobile menu

--- blitz-app/src/app/mobile-menu/page.tsx
+++ blitz-app/src/app/mobile-menu/page.tsx
@@ -14,6 +14,7 @@ import {
   MENU_ITEM_STANDARD_CLASSES,
   TOP_NAV_STANDARD_CLASSES,
 } from 'src/core/components/page-wrapper/TopNavSubmenu'
+import { ThemeProvider } from 'src/core/theme/ThemeContext'
 
 const MOBILE_LINK_CLASSES =
   'block rounded-lg bg-white p-4 py-2 text-lg text-gray-700 shadow hover:text-gray-900'
@@ -102,86 +103,88 @@ const MobileMenuPage = () => {
   }
 
   return (
-    <div className="fixed inset-0 flex flex-col bg-ladderly-light-purple-1">
-      <TopNavFlexContainer>
-        <TopNavLeft />
-        <button
-          onClick={handleClose}
-          className={`${TOP_NAV_STANDARD_CLASSES} ml-auto`}
-        >
-          Close
-        </button>
-      </TopNavFlexContainer>
+    <ThemeProvider>
+      <div className="fixed inset-0 flex flex-col bg-ladderly-light-purple-1">
+        <TopNavFlexContainer>
+          <TopNavLeft />
+          <button
+            onClick={handleClose}
+            className={`${TOP_NAV_STANDARD_CLASSES} ml-auto`}
+          >
+            Close
+          </button>
+        </TopNavFlexContainer>
 
-      <nav className="mt-25% grow overflow-y-auto px-4">
-        <ul className="grid grid-cols-2 gap-4">
-          <Suspense fallback={<p>Loading...</p>}>
-            <AuthenticatedPartialMenu />
-          </Suspense>
+        <nav className="mt-25% grow overflow-y-auto px-4">
+          <ul className="grid grid-cols-2 gap-4">
+            <Suspense fallback={<p>Loading...</p>}>
+              <AuthenticatedPartialMenu />
+            </Suspense>
 
-          <li>
-            <Link href="/about" className={MOBILE_LINK_CLASSES}>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog" className={MOBILE_LINK_CLASSES}>
-              Blog
-            </Link>
-          </li>
-          <li>
-            <button
-              onClick={toggleCommunitySubmenu}
-              className={`${MOBILE_LINK_CLASSES} flex items-center justify-between ${
-                isCommunitySubmenuOpen
-                  ? 'border border-gray-200 bg-gray-100'
-                  : ''
-              }`}
-            >
-              Community
-              <IconVerticalChevron isPointingUp={isCommunitySubmenuOpen} />
-            </button>
-            {isCommunitySubmenuOpen && (
-              <ul>
-                <CommunityMenuItems
-                  linkClassName={MOBILE_SUBMENU_ITEM_CLASSES}
-                />
-              </ul>
-            )}
-          </li>
-          <li>
-            <button
-              onClick={toggleTopHonorsSubmenu}
-              className={`${MOBILE_LINK_CLASSES} flex items-center justify-between ${
-                isTopHonorsSubmenuOpen
-                  ? 'border border-gray-200 bg-gray-100'
-                  : ''
-              }`}
-            >
-              Top Honors
-              <IconVerticalChevron isPointingUp={isTopHonorsSubmenuOpen} />
-            </button>
-            {isTopHonorsSubmenuOpen && (
-              <ul>
-                <li>
-                  <Link href="/vote" className={MOBILE_SUBMENU_ITEM_CLASSES}>
-                    Vote
-                  </Link>
-                </li>
-                <li>
-                  <Link
-                    href="/top-honors"
-                    className={MOBILE_SUBMENU_ITEM_CLASSES}
-                  >
-                    View Top Honors
-                  </Link>
-                </li>
-              </ul>
-            )}
-          </li>
-        </ul>
-      </nav>
-    </div>
+            <li>
+              <Link href="/about" className={MOBILE_LINK_CLASSES}>
+                About
+              </Link>
+            </li>
+            <li>
+              <Link href="/blog" className={MOBILE_LINK_CLASSES}>
+                Blog
+              </Link>
+            </li>
+            <li>
+              <button
+                onClick={toggleCommunitySubmenu}
+                className={`${MOBILE_LINK_CLASSES} flex items-center justify-between ${
+                  isCommunitySubmenuOpen
+                    ? 'border border-gray-200 bg-gray-100'
+                    : ''
+                }`}
+              >
+                Community
+                <IconVerticalChevron isPointingUp={isCommunitySubmenuOpen} />
+              </button>
+              {isCommunitySubmenuOpen && (
+                <ul>
+                  <CommunityMenuItems
+                    linkClassName={MOBILE_SUBMENU_ITEM_CLASSES}
+                  />
+                </ul>
+              )}
+            </li>
+            <li>
+              <button
+                onClick={toggleTopHonorsSubmenu}
+                className={`${MOBILE_LINK_CLASSES} flex items-center justify-between ${
+                  isTopHonorsSubmenuOpen
+                    ? 'border border-gray-200 bg-gray-100'
+                    : ''
+                }`}
+              >
+                Top Honors
+                <IconVerticalChevron isPointingUp={isTopHonorsSubmenuOpen} />
+              </button>
+              {isTopHonorsSubmenuOpen && (
+                <ul>
+                  <li>
+                    <Link href="/vote" className={MOBILE_SUBMENU_ITEM_CLASSES}>
+                      Vote
+                    </Link>
+                  </li>
+                  <li>
+                    <Link
+                      href="/top-honors"
+                      className={MOBILE_SUBMENU_ITEM_CLASSES}
+                    >
+                      View Top Honors
+                    </Link>
+                  </li>
+                </ul>
+              )}
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </ThemeProvider>
   )
 }
 
