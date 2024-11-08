509cace299494dffcb69ebb778efcd594d795413
Vandivier <john@ladderly.io>
2024-11-08 16:47:13 -0500

    feat: simpler t3 home page

--- ladderly-io/src/app/page.tsx
+++ ladderly-io/src/app/page.tsx
@@ -1,5 +1,3 @@
-import Link from "next/link";
-
 import { AuthButtons } from "~/app/_components/AuthButtons";
 import { getServerAuthSession } from "~/server/auth";
 import { api } from "~/trpc/server";
@@ -15,30 +13,6 @@ export default async function Home() {
           Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
         </h1>
 
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/usage/first-steps"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">First Steps →</h3>
-            <div className="text-lg">
-              Just the basics - Everything you need to know to set up your
-              database and authentication.
-            </div>
-          </Link>
-          <Link
-            className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-            href="https://create.t3.gg/en/introduction"
-            target="_blank"
-          >
-            <h3 className="text-2xl font-bold">Documentation →</h3>
-            <div className="text-lg">
-              Learn more about Create T3 App, the libraries it uses, and how to
-              deploy it.
-            </div>
-          </Link>
-        </div>
         <div className="flex flex-col items-center gap-2">
           <p className="text-2xl text-white">
             {hello ? hello.greeting : "Loading tRPC query..."}
