6e6db635489db32e8a7e6971579191678f2cd5ee
Vandivier <john@ladderly.io>
2024-11-08 22:54:33 -0500

    feat: page wrapper doesn't use blitz

--- ladderly-io/src/app/core/components/page-wrapper/LadderlyPageWrapper.tsx
+++ ladderly-io/src/app/core/components/page-wrapper/LadderlyPageWrapper.tsx
@@ -1,69 +1,66 @@
 import Link from "next/link";
 import React from "react";
 
-import Layout, { LayoutProps } from "../../layouts/Layout";
 import { TopNav } from "./TopNav";
 
 import styles from "src/styles/Home.module.css";
 
-export const LadderlyPageWrapper: React.FC<LayoutProps> = ({
+export const LadderlyPageWrapper: React.FC<{ children: React.ReactNode }> = ({
   children,
-  slug = "",
-  title,
+}: {
+  children: React.ReactNode;
 }) => (
-  <Layout slug={slug} title={title}>
-    <div className={styles.container}>
-      <TopNav />
+  <div className={styles.container}>
+    <TopNav />
 
-      <div className={styles.globe} />
+    <div className={styles.globe} />
 
-      {children}
+    {children}
 
-      <footer className={styles.footer}>
-        <ul className={styles["footer-links"]}>
-          <li>
-            <Link href="/about" className={styles.textLink}>
-              About
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://discord.gg/fAg6Xa4uxc"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.textLink}
-            >
-              Discord
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://github.com/Vandivier/ladderly-3"
-              target="_blank"
-              rel="noopener noreferrer"
-              className={styles.textLink}
-            >
-              GitHub
-            </Link>
-          </li>
-          <li>
-            <Link href="/privacy-policy" className={styles.textLink}>
-              Privacy Policy
-            </Link>
-          </li>
-        </ul>
-        <p>
-          Copyright © {new Date().getFullYear()}{" "}
+    <footer className={styles.footer}>
+      <ul className={styles["footer-links"]}>
+        <li>
+          <Link href="/about" className={styles.textLink}>
+            About
+          </Link>
+        </li>
+        <li>
           <Link
-            href="https://vandivier.github.io/not-johns-linktree/"
+            href="https://discord.gg/fAg6Xa4uxc"
             target="_blank"
             rel="noopener noreferrer"
             className={styles.textLink}
           >
-            John Vandivier
+            Discord
           </Link>
-        </p>
-      </footer>
-    </div>
-  </Layout>
+        </li>
+        <li>
+          <Link
+            href="https://github.com/Vandivier/ladderly-3"
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.textLink}
+          >
+            GitHub
+          </Link>
+        </li>
+        <li>
+          <Link href="/privacy-policy" className={styles.textLink}>
+            Privacy Policy
+          </Link>
+        </li>
+      </ul>
+      <p>
+        Copyright © {new Date().getFullYear()}{" "}
+        <Link
+          href="https://vandivier.github.io/not-johns-linktree/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.textLink}
+        >
+          John Vandivier
+        </Link>
+      </p>
+    </footer>
+  </div>
 );
