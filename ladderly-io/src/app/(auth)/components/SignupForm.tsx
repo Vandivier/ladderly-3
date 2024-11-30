a7903b506af174b73aea28fe15659b43188461eb
Vandivier <john@ladderly.io>
2024-11-30 13:46:38 -0500

    feat: broken signup migration

---
+++ ladderly-io/src/app/(auth)/components/SignupForm.tsx
@@ -0,0 +1,64 @@
+/* TODO: migrate from blitzjs to tRPC */
+
+"use client";
+import { useMutation } from "@blitzjs/rpc";
+import Link from "next/link";
+import signup from "src/app/(auth)/mutations/signup";
+import { Signup } from "src/app/(auth)/schemas";
+import { Form, FORM_ERROR } from "src/core/components/Form";
+import { LabeledTextField } from "src/core/components/LabeledTextField";
+
+type SignupFormProps = {
+  onSuccess?: () => void;
+};
+
+export const SignupForm = (props: SignupFormProps) => {
+  const [signupMutation] = useMutation(signup);
+  return (
+    <div className="w-full max-w-md rounded-lg bg-white p-8 shadow-md">
+      <h1 className="mb-4 text-2xl font-bold text-gray-800">
+        Create an Account
+      </h1>
+
+      <Form
+        submitText="Create Account"
+        schema={Signup}
+        initialValues={{ email: "", password: "" }}
+        className="space-y-4"
+        onSubmit={async (values) => {
+          try {
+            await signupMutation(values);
+            props.onSuccess?.();
+          } catch (error: any) {
+            if (
+              error.code === "P2002" &&
+              error.meta?.target?.includes("email")
+            ) {
+              // This error comes from Prisma
+              return { email: "This email is already being used" };
+            } else {
+              return { [FORM_ERROR]: error.toString() };
+            }
+          }
+        }}
+      >
+        <LabeledTextField name="email" label="Email" placeholder="Email" />
+        <LabeledTextField
+          name="password"
+          label="Password"
+          placeholder="Password"
+          type="password"
+        />
+      </Form>
+
+      <div className="mt-4">
+        Already signed up?{" "}
+        <Link className="underline" href="/login">
+          Log in here!
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default SignupForm;
---
+++ ladderly-io/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,32 @@
+/* TODO: migrate from blitzjs to tRPC */
+
+"use client";
+import { BlitzPage } from "@blitzjs/next";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { SignupForm } from "src/app/(auth)/components/SignupForm";
+import Layout from "src/core/layouts/Layout";
+
+const CreateAccountPage: BlitzPage = () => {
+  const router = useRouter();
+
+  return (
+    <Layout title="Create Account">
+      <div className="relative min-h-screen">
+        <nav className="border-ladderly-light-purple flex border bg-ladderly-off-white px-4 py-1 text-ladderly-violet-700">
+          <Link
+            href="/"
+            className="ml-auto text-gray-800 hover:text-ladderly-pink"
+          >
+            Back to Home
+          </Link>
+        </nav>
+        <div className="flex min-h-[calc(100vh-4rem)] items-center justify-center">
+          <SignupForm onSuccess={() => router.push("/")} />
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default CreateAccountPage;
