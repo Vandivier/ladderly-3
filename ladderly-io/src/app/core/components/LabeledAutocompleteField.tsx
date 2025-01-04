e40c37a0ded8e40f7c74f95124d1bdf1255e4199
Vandivier <john@ladderly.io>
2025-01-03 22:05:53 -0500

    feat: simpler autocomplete

--- ladderly-io/src/app/core/components/LabeledAutocompleteField.tsx
+++ ladderly-io/src/app/core/components/LabeledAutocompleteField.tsx
@@ -15,119 +15,50 @@ const LabeledAutocompleteField: React.FC<LabeledAutocompleteFieldProps> = ({
   options,
   outerProps,
 }) => {
-  const [isOpen, setIsOpen] = React.useState(false)
-
-  const handleClick = (e: React.MouseEvent) => {
-    e.preventDefault()
-    e.stopPropagation()
-    console.log('click detected')
-    setIsOpen(true)
-  }
-
-  const handleTouch = () => {
-    console.log('touch detected')
-    // Force menu to open in next tick
-    setTimeout(() => {
-      setIsOpen(true)
-    }, 0)
-  }
-
   return (
     <div {...outerProps}>
       <label htmlFor={name}>{label}</label>
-      <input
-        type="text"
-        name={`${name}-autocomplete-killer`}
-        style={{ display: 'none' }}
-        autoComplete="chrome-off"
-      />
       <Field name={name}>
         {({ input }) => (
-          <div role="button" tabIndex={0} className="relative mt-1">
-            <Select
-              {...input}
-              menuIsOpen={isOpen}
-              onMenuClose={() => setIsOpen(false)}
-              classNamePrefix="react-select"
-              options={options}
-              value={
-                options.find((option) => option.value === input.value) || null
-              }
-              onChange={(option) => {
-                input.onChange(option?.value ?? '')
-                setIsOpen(false)
-              }}
-              placeholder={`Select ${label}`}
-              isClearable={false}
-              isSearchable={true}
-              openMenuOnFocus={true}
-              components={{
-                Input: (props) => (
-                  <components.Input
-                    {...props}
-                    autoComplete="new-password"
-                    autoCorrect="off"
-                    spellCheck="false"
-                    aria-autocomplete="none"
-                    data-lpignore="true"
-                    data-form-type="other"
-                  />
-                ),
-                Control: ({ children, ...props }) => (
-                  <components.Control {...props}>
-                    <div
-                      className="w-full"
-                      onClick={handleClick}
-                      onTouchStart={handleTouch}
-                      role="button"
-                      tabIndex={0}
-                      onKeyDown={(e) => {
-                        if (e.key === 'Enter' || e.key === ' ') {
-                          e.preventDefault()
-                          setIsOpen(true)
-                        }
-                      }}
-                    >
-                      {children}
-                    </div>
-                  </components.Control>
-                ),
-              }}
-              styles={{
-                control: (base) => ({
-                  ...base,
+          <Select
+            {...input}
+            className="mt-1"
+            classNamePrefix="react-select"
+            options={options}
+            value={
+              options.find((option) => option.value === input.value) || null
+            }
+            onChange={(option) => input.onChange(option?.value ?? '')}
+            placeholder={`Select ${label}`}
+            isClearable={false}
+            isSearchable={true}
+            components={{
+              Input: (props) => (
+                <components.Input
+                  {...props}
+                  autoComplete="new-password"
+                  autoCorrect="off"
+                  spellCheck="false"
+                  aria-autocomplete="none"
+                  data-lpignore="true"
+                  data-form-type="other"
+                />
+              ),
+            }}
+            styles={{
+              control: (base) => ({
+                ...base,
+                borderColor: 'purple',
+                borderRadius: '4px',
+                minHeight: '38px',
+                boxShadow: 'none',
+                cursor: 'pointer',
+                '&:hover': {
                   borderColor: 'purple',
-                  borderRadius: '4px',
-                  minHeight: '38px',
-                  boxShadow: 'none',
-                  cursor: 'pointer',
-                  padding: '0',
-                  '&:hover': {
-                    borderColor: 'purple',
-                  },
-                }),
-                menu: (base) => ({
-                  ...base,
-                  zIndex: 10,
-                }),
-                input: (base) => ({
-                  ...base,
-                  cursor: 'pointer',
-                  padding: '8px',
-                }),
-                singleValue: (base) => ({
-                  ...base,
-                  cursor: 'pointer',
-                  padding: '8px',
-                }),
-                placeholder: (base) => ({
-                  ...base,
-                  cursor: 'pointer',
-                  padding: '8px',
-                }),
-              }}
-            />
-          </div>
+                },
+              }),
+            }}
+          />
         )}
       </Field>
     </div>
