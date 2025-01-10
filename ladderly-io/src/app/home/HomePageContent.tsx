03b4e7acf424ebad492efd27f9d992e1fe16ca0c
Vandivier <john@ladderly.io>
2025-01-09 22:17:20 -0500

    style: more consistent padding
    and section margin

--- ladderly-io/src/app/home/HomePageContent.tsx
+++ ladderly-io/src/app/home/HomePageContent.tsx
@@ -1,57 +1,57 @@
 // src/app/HomePageContent.tsx
 
-import { PaymentTierEnum } from "@prisma/client";
-import Image from "next/image";
-import Link from "next/link";
+import { PaymentTierEnum } from '@prisma/client'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import React from "react";
-import { LadderlyPageWrapper } from "~/app/core/components/page-wrapper/LadderlyPageWrapper";
-import PricingGrid from "~/app/core/components/pricing-grid/PricingGrid";
-import type { LadderlySession } from "~/server/auth";
-import { LadderlyHelpsBlock } from "./LadderlyHelpsBlock";
-import { TestimonialBlock } from "./TestimonialBlock";
+import React from 'react'
+import { LadderlyPageWrapper } from '~/app/core/components/page-wrapper/LadderlyPageWrapper'
+import PricingGrid from '~/app/core/components/pricing-grid/PricingGrid'
+import type { LadderlySession } from '~/server/auth'
+import { LadderlyHelpsBlock } from './LadderlyHelpsBlock'
+import { TestimonialBlock } from './TestimonialBlock'
 
-import styles from "~/styles/Home.module.css";
+import styles from '~/styles/Home.module.css'
 
 // note: do not extract `DARK_MODE_STANDARD_CLASSES` out of file.
 // it is duplicated intentionally between files to ensure tailwind classes are bundled
 const DARK_MODE_STANDARD_CLASSES =
-  "dark:bg-ladderly-dark-purple-2 dark:text-white";
+  'dark:bg-ladderly-dark-purple-2 dark:text-white'
 
 const HomePageCardSubheading = ({
   children,
 }: {
-  children: React.ReactNode;
-}) => <h2 className="mb-2 text-xl font-bold">{children}</h2>;
+  children: React.ReactNode
+}) => <h2 className="mb-2 text-xl font-bold">{children}</h2>
 
 const AdvancedChecklistContentBlock = ({
   session,
 }: {
-  session: LadderlySession | null;
+  session: LadderlySession | null
 }) => {
-  const isPaid = session?.user?.subscription.tier !== PaymentTierEnum.FREE;
+  const isPaid = session?.user?.subscription.tier !== PaymentTierEnum.FREE
 
   return isPaid ? (
     <div
       className={`m-2 w-[300px] rounded-lg bg-white p-2 shadow-lg lg:w-auto`}
     >
       <p className="text-gray-800">
-        As a paid member, you can access the{" "}
+        As a paid member, you can access the{' '}
         <Link
           className="text-m font-bold text-ladderly-pink hover:underline"
-          href={"/checklists/my-premium-checklist"}
+          href={'/checklists/my-premium-checklist'}
         >
           Advanced Checklist
         </Link>
         !
       </p>
     </div>
-  ) : null;
-};
+  ) : null
+}
 
 const HomePageContent = ({ session }: { session: LadderlySession | null }) => (
   <LadderlyPageWrapper>
-    <main style={{ padding: "0rem 1rem" }}>
+    <main style={{ padding: '0rem 1rem' }}>
       <div className={styles.wrapper}>
         <div
           className={`mx-auto flex flex-wrap gap-0 rounded-lg bg-frost p-2 sm:flex-nowrap sm:gap-16`}
@@ -62,7 +62,7 @@ const HomePageContent = ({ session }: { session: LadderlySession | null }) => (
             height={270}
             priority={true}
             src="/logo.png"
-            style={{ alignSelf: "center", maxHeight: "160px" }}
+            style={{ alignSelf: 'center', maxHeight: '160px' }}
             width={270}
           />
 
@@ -72,18 +72,18 @@ const HomePageContent = ({ session }: { session: LadderlySession | null }) => (
             className="m-6 hidden rounded-lg shadow-lg sm:block"
             priority={true}
             src="/logo.png"
-            style={{ alignSelf: "center", maxHeight: "200px" }}
+            style={{ alignSelf: 'center', maxHeight: '200px' }}
             width={330}
           />
 
           <LadderlyHelpsBlock />
         </div>
 
-        <div className="flex flex-col items-center">
+        <div className="mt-2 flex flex-col items-center">
           <div className="flex flex-col justify-center sm:mt-4 sm:flex-row">
             <section id="testimonials">
               <div
-                className={`text-gray-800 ${DARK_MODE_STANDARD_CLASSES} ${styles["next-steps-card"]} rounded-lg bg-white p-6 shadow-lg`}
+                className={`text-gray-800 ${DARK_MODE_STANDARD_CLASSES} ${styles['next-steps-card']} rounded-lg bg-white p-6 shadow-lg`}
               >
                 <HomePageCardSubheading>
                   Why Users Love Us:
@@ -94,23 +94,23 @@ const HomePageContent = ({ session }: { session: LadderlySession | null }) => (
 
             <section id="recommended-next-steps" className="flex flex-col">
               <div
-                className={`text-gray-800 ${DARK_MODE_STANDARD_CLASSES} ${styles["next-steps-card"]} rounded-lg bg-white p-6 shadow-lg`}
+                className={`text-gray-800 ${DARK_MODE_STANDARD_CLASSES} ${styles['next-steps-card']} rounded-lg bg-white p-6 shadow-lg`}
               >
                 <HomePageCardSubheading>
                   Recommended Next Steps:
                 </HomePageCardSubheading>
                 <p className="text-l font-bold">
-                  Complete the{" "}
+                  Complete the{' '}
                   <Link
                     className="text-ladderly-pink hover:underline"
-                    href={"/checklists/my-basic-checklist"}
+                    href={'/checklists/my-basic-checklist'}
                   >
                     Standard Checklist
                   </Link>
-                  , consider one of the paid plans below, and{" "}
+                  , consider one of the paid plans below, and{' '}
                   <Link
                     className="text-ladderly-pink hover:underline"
-                    href={"https://buy.stripe.com/cN2bMfbOQ2CX5dC7su"}
+                    href={'https://buy.stripe.com/cN2bMfbOQ2CX5dC7su'}
                     target="_blank"
                   >
                     Book an Expert Session
@@ -120,14 +120,14 @@ const HomePageContent = ({ session }: { session: LadderlySession | null }) => (
               </div>
 
               <div
-                className={`text-gray-800 ${DARK_MODE_STANDARD_CLASSES} ${styles["next-steps-card"]} rounded-lg bg-white p-2 shadow-lg`}
+                className={`text-gray-800 ${DARK_MODE_STANDARD_CLASSES} ${styles['next-steps-card']} rounded-lg bg-white p-2 shadow-lg`}
               >
                 <p>
                   To support Ladderly{"'"}s mission to provide low-cost
-                  education in STEM, consider{" "}
+                  education in STEM, consider{' '}
                   <Link
                     className="text-m font-bold text-ladderly-pink hover:underline"
-                    href={"https://buy.stripe.com/eVa9E72egelFfSgfYZ"}
+                    href={'https://buy.stripe.com/eVa9E72egelFfSgfYZ'}
                     target="_blank"
                   >
                     leaving a tip
@@ -144,6 +144,6 @@ const HomePageContent = ({ session }: { session: LadderlySession | null }) => (
       </div>
     </main>
   </LadderlyPageWrapper>
-);
+)
 
-export default HomePageContent;
+export default HomePageContent
