9c0ca28888bc84dba25a2a77213a541a1fd714f7
Vandivier <vandivi3r@gmail.com>
2025-02-07 22:25:50 -0500

    fix: add id

--- ladderly-io/src/app/home/HomePageContent.tsx
+++ ladderly-io/src/app/home/HomePageContent.tsx
@@ -70,7 +70,7 @@ const HomePageContent = ({ session }: { session: LadderlySession | null }) => (
         </Link>
       </h2>
 
-      <Script>
+      <Script id="chatbase-script">
         {`
         // ref: https://www.chatbase.co/docs/developer-guides/identity-verification
         (function(){if(!window.chatbase||window.chatbase("getState")!=="initialized"){window.chatbase=(...arguments)=>{if(!window.chatbase.q){window.chatbase.q=[]}window.chatbase.q.push(arguments)};window.chatbase=new Proxy(window.chatbase,{get(target,prop){if(prop==="q"){return target.q}return(...args)=>target(prop,...args)}})}const onLoad=function(){const script=document.createElement("script");script.src="https://www.chatbase.co/embed.min.js";script.id="NnbDY2pCxZROnVKZwpjaW";script.domain="www.chatbase.co";document.body.appendChild(script)};if(document.readyState==="complete"){onLoad()}else{window.addEventListener("load",onLoad)}})();
