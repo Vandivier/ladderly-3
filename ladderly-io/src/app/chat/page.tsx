44cb64e9c7da50cf55f0aeeb968b6ac3f59fc0f7
Vandivier <john@ladderly.io>
2025-02-16 19:35:28 -0500

    feat: premium gate chat

--- ladderly-io/src/app/chat/page.tsx
+++ ladderly-io/src/app/chat/page.tsx
@@ -3,9 +3,13 @@ import { LadderlyPageWrapper } from '~/app/core/components/page-wrapper/Ladderly
 import { ClientChat } from './ClientChat'
 import { getServerAuthSession } from '~/server/auth'
 import Link from 'next/link'
+import { PaymentTierEnum } from '@prisma/client'
+
+const STRIPE_PREMIUM_PAYMENT_LINK = 'https://buy.stripe.com/6oE7vZdWY3H18pO6ov'
 
 export default async function ChatPage() {
   const session = await getServerAuthSession()
+  const user = session?.user
 
   if (!session) {
     return (
@@ -13,14 +17,44 @@ export default async function ChatPage() {
         <div className="container mx-auto max-w-3xl px-4 py-8">
           <p className="text-center text-gray-800 dark:text-gray-200">
             Please{' '}
-            <Link href="/signup" className="text-blue-600 hover:underline">
+            <Link
+              href="/signup?redirect=chat"
+              className="text-blue-600 hover:underline"
+            >
               sign up
             </Link>{' '}
             or{' '}
-            <Link href="/login" className="text-blue-600 hover:underline">
+            <Link
+              href="/login?redirect=chat"
+              className="text-blue-600 hover:underline"
+            >
               log in
             </Link>{' '}
             to use Ladderly Chat.
+          </p>
+        </div>
+      </LadderlyPageWrapper>
+    )
+  }
+
+  if (user && user.subscription.tier !== PaymentTierEnum.PREMIUM) {
+    return (
+      <LadderlyPageWrapper>
+        <div className="container mx-auto max-w-3xl px-4 py-8">
+          <p className="text-center text-gray-800 dark:text-gray-200">
+            A premium subscription is required to use Ladderly Chat. Please{' '}
+            <Link
+              href={{
+                pathname: STRIPE_PREMIUM_PAYMENT_LINK,
+                query: {
+                  client_reference_id: user.id.toString(),
+                },
+              }}
+              className="text-blue-600 hover:underline"
+            >
+              upgrade to premium
+            </Link>
+            .
           </p>
         </div>
       </LadderlyPageWrapper>
