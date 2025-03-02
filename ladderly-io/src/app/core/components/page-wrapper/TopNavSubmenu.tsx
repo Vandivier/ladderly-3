f63ac68571dc7524d98d4dcb33f1f6df6126c2ba
Vandivier <vandivi3r@gmail.com>
2025-03-02 11:32:49 -0800

    fix: invisible subnav text
    in dark mode

--- ladderly-io/src/app/core/components/page-wrapper/TopNavSubmenu.tsx
+++ ladderly-io/src/app/core/components/page-wrapper/TopNavSubmenu.tsx
@@ -1,22 +1,21 @@
-"use client";
+'use client'
 
-import { signOut } from "next-auth/react";
-// import { User } from "@prisma/client";
-import Link from "next/link";
-import React from "react";
+import { signOut } from 'next-auth/react'
+import Link from 'next/link'
+import React from 'react'
 
-import { MenuContext } from "./MenuProvider";
+import { MenuContext } from './MenuProvider'
 
-export const TOP_NAV_STANDARD_CLASSES = "ml-6 font-bold";
+export const TOP_NAV_STANDARD_CLASSES = 'ml-6 font-bold'
 export const MENU_ITEM_STANDARD_CLASSES =
-  "font-semibold block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:text-gray-100 dark:hover:bg-purple-300/20";
+  'font-semibold block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900 dark:hover:bg-purple-300/20'
 
 export const AccountMenuItems = ({
   userId,
   linkClassName = MENU_ITEM_STANDARD_CLASSES,
 }: {
-  userId: string;
-  linkClassName?: string;
+  userId: string
+  linkClassName?: string
 }) =>
   userId ? (
     <MenuItemsWrapper>
@@ -28,7 +27,7 @@ export const AccountMenuItems = ({
       </Link>
       <LogoutButton className={linkClassName} />
     </MenuItemsWrapper>
-  ) : null;
+  ) : null
 
 const MenuItemsWrapper = ({ children }: { children: React.ReactNode }) => (
   <div
@@ -39,26 +38,17 @@ const MenuItemsWrapper = ({ children }: { children: React.ReactNode }) => (
   >
     {children}
   </div>
-);
+)
 
 export const CommunityMenuItems = ({
   linkClassName = MENU_ITEM_STANDARD_CLASSES,
 }: {
-  linkClassName?: string;
+  linkClassName?: string
 }) => (
   <MenuItemsWrapper>
-    {/* <Link href="/community/hall-of-fame" className={linkClassName}>
-      Hall of Fame
-    </Link> */}
     <Link href="/community" className={linkClassName}>
       Browse Profiles
     </Link>
-    {/* <Link href="/questions" className={linkClassName}>
-      Questions
-    </Link>
-    <Link href="/events-calendar" className={linkClassName}>
-      Events Calendar
-    </Link> */}
     <Link
       href="https://discord.com/invite/fAg6Xa4uxc"
       className={linkClassName}
@@ -67,37 +57,22 @@ export const CommunityMenuItems = ({
       Discord
     </Link>
   </MenuItemsWrapper>
-);
+)
 
 const LogoutButton = ({ className }: { className: string }) => {
-  const { setMenu } = React.useContext(MenuContext);
+  const { setMenu } = React.useContext(MenuContext)
 
   return (
     <button
       className={className}
       onClick={() => {
-        setMenu?.(null, "");
+        setMenu?.(null, '')
         void signOut({
-          callbackUrl: "/",
-        });
+          callbackUrl: '/',
+        })
       }}
     >
       Log Out
     </button>
-  );
-};
-
-// export const TopHonorsMenuItems = ({
-//   linkClassName = MENU_ITEM_STANDARD_CLASSES,
-// }: {
-//   linkClassName?: string;
-// }) => (
-//   <MenuItemsWrapper>
-//     <Link href="/top-honors" className={linkClassName}>
-//       View Leaders
-//     </Link>
-//     <Link href="/vote" className={linkClassName}>
-//       Vote
-//     </Link>
-//   </MenuItemsWrapper>
-// );
+  )
+}
