a79cf2876742d3dc568522934a08e0d3e7bd1049
Vandivier <john@ladderly.io>
2025-03-15 19:32:22 -0400

    feat: how to go public

--- ladderly-io/src/app/community/page.tsx
+++ ladderly-io/src/app/community/page.tsx
@@ -1,5 +1,7 @@
 // src/app/community/page.tsx
 
+import { Info } from 'lucide-react'
+import Link from 'next/link'
 import { Suspense } from 'react'
 import { SmallCard } from '~/app/core/components/SmallCard'
 import { LadderlyPageWrapper } from '~/app/core/components/page-wrapper/LadderlyPageWrapper'
@@ -14,6 +16,15 @@ export default function CommunityPage() {
     <LadderlyPageWrapper>
       <SmallCard className="mx-4 mt-4">
         <h1 className="mb-6 text-2xl font-bold">Community Member Profiles</h1>
+        <div className="flex items-center gap-2">
+          <Info className="size-4" />
+          <p className="text-sm">
+            Set your profile to public from the{' '}
+            <Link className="underline" href="/settings">
+              Settings Page
+            </Link>
+          </p>
+        </div>
 
         <Suspense fallback="Loading...">
           <ClientCommunityPage />
