06f0774fa31453f4134bd8d84d630bb8e2bd02ac
Vandivier <john@ladderly.io>
2025-03-25 20:35:16 -0400

    feat: track clicks on reimbursement letter

--- ladderly-io/src/app/core/components/pricing-grid/PricingGrid.tsx
+++ ladderly-io/src/app/core/components/pricing-grid/PricingGrid.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link'
 import React from 'react'
 import type { UserWithSubscriptionsOrZero } from '~/server/schemas'
 import { api } from '~/trpc/server'
+import { event } from 'nextjs-google-analytics'
 
 type Benefit = {
   paragraphContent?: React.ReactNode
@@ -166,6 +167,12 @@ const PricingGrid: React.FC = async () => {
           <Link
             className="text-m font-bold text-ladderly-pink hover:underline"
             href="https://docs.google.com/document/d/1DtwRvBRimmSiuQ-jkKo_P9QNBPLKQkFemR9vT_Kl9Jg"
+            onClick={() => {
+              event('click', {
+                category: 'Link',
+                label: 'Reimbursment Request Letter Link Click',
+              })
+            }}
             target="_blank"
           >
             Reimbursement Request Letter
