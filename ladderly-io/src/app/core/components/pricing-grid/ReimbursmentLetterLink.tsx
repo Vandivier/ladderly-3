83046b8f2f004de0ab3a6ee0ae205f8b6c691156
Vandivier <john@ladderly.io>
2025-03-25 20:54:16 -0400

    feat: use server
    after all

--- ladderly-io/src/app/core/components/pricing-grid/ReimbursmentLetterLink.tsx
+++ ladderly-io/src/app/core/components/pricing-grid/ReimbursmentLetterLink.tsx
@@ -1,19 +1,12 @@
-'use client'
+'use server'
 
 import Link from 'next/link'
-import { event } from 'nextjs-google-analytics'
 
 export const ReimbursmentLetterLink = () => (
   <Link
     className="text-m font-bold text-ladderly-pink hover:underline"
     href="https://docs.google.com/document/d/1DtwRvBRimmSiuQ-jkKo_P9QNBPLKQkFemR9vT_Kl9Jg"
     id="reimbursment-letter-link"
-    onClick={() => {
-      event('click', {
-        category: 'Link',
-        label: 'Reimbursment Request Letter Link Click',
-      })
-    }}
     target="_blank"
   >
     Reimbursement Request Letter
