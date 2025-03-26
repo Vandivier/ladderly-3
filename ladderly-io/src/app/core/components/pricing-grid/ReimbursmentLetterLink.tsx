36bbd586afc7c4bf0027cd6f456c0fd9f2c8b01a
Vandivier <john@ladderly.io>
2025-03-25 20:48:30 -0400

    feat: report ID on link

--- ladderly-io/src/app/core/components/pricing-grid/ReimbursmentLetterLink.tsx
+++ ladderly-io/src/app/core/components/pricing-grid/ReimbursmentLetterLink.tsx
@@ -7,6 +7,7 @@ export const ReimbursmentLetterLink = () => (
   <Link
     className="text-m font-bold text-ladderly-pink hover:underline"
     href="https://docs.google.com/document/d/1DtwRvBRimmSiuQ-jkKo_P9QNBPLKQkFemR9vT_Kl9Jg"
+    id="reimbursment-letter-link"
     onClick={() => {
       event('click', {
         category: 'Link',
