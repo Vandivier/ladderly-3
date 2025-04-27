f36cc94ecbb1507beca45d74ae7fb923b9961963
Vandivier <vandivi3r@gmail.com>
2025-04-27 15:14:43 -0400

    fix: dark mode text legibility

--- ladderly-io/src/app/core/components/LadderlyPitch.tsx
+++ ladderly-io/src/app/core/components/LadderlyPitch.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export const LadderlyPitch = () => (
   <div className="container mx-auto max-w-3xl px-4 py-8">
-    <p className="text-center text-gray-800 dark:text-gray-200">
+    <p className="text-center text-gray-800">
       Please{' '}
       <Link href="/signup" className="text-blue-600 hover:underline">
         sign up
@@ -14,7 +14,7 @@ export const LadderlyPitch = () => (
       to continue.
     </p>
 
-    <p className="mt-4 text-center text-gray-800 dark:text-gray-200">
+    <p className="mt-4 text-center text-gray-800">
       Ladderly.io is available for free. Sign up for{' '}
       <Link
         className="text-blue-600 hover:underline"
