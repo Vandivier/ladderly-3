e297fcb4f74fe7fcb1b5c6a468b1e9e332aec1cc
Vandivier <vandivi3r@gmail.com>
2025-11-27 19:17:47 -0500

    test: vitest.config.integration.ts

--- ladderly-io/tests/setup-fetch.ts
+++
@@ -1,9 +0,0 @@
-import fetch, { Headers } from 'node-fetch'
-
-if (typeof globalThis.fetch !== 'function') {
-  globalThis.fetch = fetch as unknown as typeof globalThis.fetch
-}
-
-if (typeof globalThis.Headers !== 'function') {
-  globalThis.Headers = Headers as unknown as typeof globalThis.Headers
-}
--- ladderly-io/vitest.config.ts
+++ ladderly-io/vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./tests/setup-fetch.ts', './tests/setup.tsx'],
+    setupFiles: ['./tests/setup.tsx'],
     pool: 'threads',
     poolOptions: {
       threads: {
